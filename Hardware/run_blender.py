64f1e54269c7ff762443c209b807b97a6ab82680
lk <k.lukas@posteo.de>
2022-07-24 10:22:30 +0200

    alpha done :)

---
+++ Hardware/run_blender.py
@@ -0,0 +1,29 @@
+import bpy
+import json
+
+
+# read parameters
+configFile =  bpy.path.abspath("//par_file.json")
+
+print(configFile)
+
+with open(configFile, 'r') as f:
+  data = json.load(f)
+
+print(data)
+
+length = data["lenght"]
+width = data["width"]
+height = data["height"]
+
+
+# set parameters
+bpy.ops.transform.resize( value=(length,width,height))
+
+
+# render image
+bpy.context.scene.render.filepath = "//images/image.png"
+print(bpy.path.abspath(bpy.context.scene.render.filepath))
+bpy.context.scene.render.resolution_x = 640 
+bpy.context.scene.render.resolution_y = 480
+bpy.ops.render.render(write_still = True)
